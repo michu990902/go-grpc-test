d6ef3b00d9456a15991e2c1c48f475b29e491167
michu990902 <41807744+michu990902@users.noreply.github.com>
2020-10-06 22:54:38 +0200

    greet unary api server added

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
 
@@ -9,6 +11,16 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
 	// fmt.Println("test")
